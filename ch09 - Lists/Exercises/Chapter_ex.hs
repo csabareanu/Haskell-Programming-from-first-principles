221d5512a98069f5d722336bf59f0b4669721cef
Ciprian Sabareanu <ciprian.sabareanu@opti.ro>
2018-07-24 17:45:39 +0300

    Chapter 9 Final Exercises

--- ch09 - Lists/Exercises/Chapter_ex.hs
+++ ch09 - Lists/Exercises/Chapter_ex.hs
@@ -53,3 +53,118 @@ capitalizeFirstComp s = toUpper . head $ s
 
 capitalizeFirstPF :: [Char] -> Char
 capitalizeFirstPF = toUpper . head
+
+----------------------------------------
+-- Writing your own standard functions
+----------------------------------------
+
+-- direct recursion, not using (&&)
+myAnd :: [Bool] -> Bool
+myAnd [] = True
+myAnd (x:xs) = if x == False then False else myAnd xs
+-- direct recursion, using (&&)
+myAnd2 :: [Bool] -> Bool
+myAnd2 [] = True
+myAnd2 (x:xs) = x && myAnd xs
+
+
+-- 1. myOr returns True if any Bool in the list is True.
+myOr :: [Bool] -> Bool
+-- myOr = undefined
+myOr [] = False
+myOr (x:xs) = x || myOr xs
+
+
+-- 2. myAny returns True if a -> Bool applied to any of the values in
+-- the list returns True.
+myAny :: (a -> Bool) -> [a] -> Bool
+-- myAny = undefined
+-- myAny f l = myOr . map f $ l
+myAny _ [] = False
+myAny f (x:xs) = if f x then True else myAny f xs
+
+-- Example for validating myAny:
+-- Prelude> myAny even [1, 3, 5]
+-- False
+-- Prelude> myAny odd [1, 3, 5]
+-- True
+
+
+-- 3. After you write the recursive myElem, write another version that
+-- uses any.
+-- -- the built-in version of 'elem' in GHC 7.10
+-- -- and newer has a type that uses Foldable
+-- -- instead of the list type specifically. You
+-- -- can ignore that and write the concrete
+-- -- version that works only for list.
+myElem :: Eq a => a -> [a] -> Bool
+myElem _ [] = False
+myElem a (x:xs) = if a == x then True else myElem a xs
+
+myElemAny :: Eq a => a -> [a] -> Bool
+myElemAny a = myAny (== a)
+
+-- Prelude> myElem 1 [1..10]
+-- True
+-- Prelude> myElem 1 [2..10]
+-- False
+
+
+-- 4. Implement myReverse.
+myReverse :: [a] -> [a]
+-- myReverse = undefined
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+-- Prelude> myReverse "blah"
+-- "halb"
+-- Prelude> myReverse [1..5]
+-- [5,4,3,2,1]
+
+
+-- 5. squish flattens a list of lists into a list
+squish :: [[a]] -> [a]
+-- squish = undefined
+squish [] = []
+squish (x:xs) = x ++ squish xs
+
+
+-- 6. squishMap maps a function over a list and concatenates the results.
+squishMap :: (a -> [b]) -> [a] -> [b]
+-- squishMap = undefined
+squishMap _ [] = []
+squishMap f (x:xs) = f x ++ squishMap f xs
+
+-- Prelude> squishMap (\x -> [1, x, 3]) [2]
+-- [1,2,3]
+-- Prelude> squishMap (\x -> "WO "++[x]++" HOO ") "123"
+-- "WO 1 HOO WO 2 HOO WO 3 HOO "
+
+
+-- 7. squishAgain flattens a list of lists into a list. This time re-use
+-- the squishMap function.
+squishAgain :: [[a]] -> [a]
+-- squishAgain = undefined
+squishAgain = squishMap id
+
+-- 8. myMaximumBy takes a comparison function and a list and returns
+-- the greatest element of the list based on the last value that the
+-- comparison returned GT for.
+-- -- If you import maximumBy from Data.List,
+-- -- you'll see the type is
+-- -- Foldable t => (a -> a -> Ordering) -> t a -> a
+-- -- rather than
+-- -- (a -> a -> Ordering) -> [a] -> a
+-- -- if you have GHC 7.10 or newer. Seeing a pattern?
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+-- myMaximumBy = undefined
+myMaximumBy f (x:xs)
+-- Prelude> myMaximumBy (\_ _ -> GT) [1..10]
+-- 1
+-- Prelude> myMaximumBy (\_ _ -> LT) [1..10]
+-- 10
+-- Prelude> myMaximumBy compare [1..10]
+-- 10
+-- 9. myMinimumBy takes a comparison function and a list and returns
+-- the least element of the list based on the last value that the comparison
+-- returned LT for.
