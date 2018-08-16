112513f092655da65148091e108487c8bf6556f2
Ciprian Sabareanu <ciprian.sabareanu@opti.ro>
2018-08-16 17:30:05 +0300

    Chapter 11

--- ch11 - Algebraic DataTypes/Exercises/Chapter_ex.hs
+++ ch11 - Algebraic DataTypes/Exercises/Chapter_ex.hs
@@ -189,3 +189,178 @@ capitalizeWords = map capitalize . words
 
 -- Prelude> capitalizeWords "hello world"
 -- [("hello", "Hello"), ("world", "World")]
+
+
+-----------------------
+-- Language exercises
+-----------------------
+
+
+-- 1. Write a function that capitalizes a word.
+capitalizeWord :: String -> String
+capitalizeWord (x:xs) = toUpper x : xs
+
+
+-- Example output.
+-- Prelude> capitalizeWord "Titter"
+-- "Titter"
+-- Prelude> capitalizeWord "titter"
+-- "Titter"
+
+
+-- 2. Write a function that capitalizes sentences in a paragraph. Recognize
+-- when a new sentence has begun by checking for periods.
+-- Reuse the capitalizeWord function.
+capitalizeParagraph :: String -> String
+capitalizeParagraph = newSentence True
+    where
+        newSentence _   ""     = ""
+        newSentence new (x:xs)
+            | x == '.'           = x : newSentence True xs
+            | new && isLetter x  = toUpper x : newSentence False xs
+            | otherwise          = x : newSentence new xs
+
+
+-- Example result you should get from your function:
+-- Prelude> capitalizeParagraph "blah. woot ha."
+-- "Blah. Woot ha."
+
+
+
+-------------------
+-- Phone exercise
+-------------------
+
+
+-- This exercise by geophf originally for 1HaskellADay. Thank you for
+-- letting us use this exercise!
+-- Remember old-fashioned phone inputs for writing text where you
+-- had to press a button multiple times to get different letters to come
+-- up? You may still have to do this when you try to search for a movie
+-- to watch using your television remote control. You’re going to write
+-- code to translate sequences of button presses into strings and vice
+-- versa.
+-- So! Here is the layout of the phone:
+---- -----------------------------------------
+---- | 1 | 2 ABC | 3 DEF |
+---- _________________________________________
+---- | 4 GHI | 5 JKL | 6 MNO |
+---- -------------------------------------------
+---- | 7 PQRS | 8 TUV | 9 WXYZ |
+---- -----------------------------------------
+---- | * ^ | 0 + _ | # ., |
+-- -----------------------------------------
+-- Where star (*) gives you capitalization of the letter you’re writing to
+-- your friends, and 0 is your space bar. To represent the digit itself, you
+-- press that digit once more than the letters it represents. If you press
+-- a button one more than than is required to type the digit, it wraps
+-- around to the first letter. For example,
+-- 2 -> 'A'
+-- 22 -> 'B'
+-- 222 -> 'C'
+-- 2222 -> '2'
+-- 22222 -> 'A'
+-- So on and so forth. We’re going to kick this around.
+-- 1. Create a data structure that captures the phone layout above.
+-- The data structure should be able to express enough of how the
+-- layout works that you can use it to dictate the behavior of the
+-- functions in the following exercises.
+-- -- fill in the rest.
+type Digit = Char
+
+data DaPhone = DaPhone [(Digit, [Digit])]
+
+phone :: DaPhone
+phone = DaPhone [('1', "1")
+                 , ('2', "abc2")
+                 , ('3', "def3")
+                 , ('4', "ghi4")
+                 , ('5', "jkl5")
+                 , ('6', "mno6")
+                 , ('7', "pqrs7")
+                 , ('8', "tuv8")
+                 , ('9', "wxyz9")
+                 , ('*', "^*")
+                 , ('0', "+_0")
+                 , ('#', ".,#")
+                ]
+
+-- 2. Convert the following conversations into the keypresses required
+-- to express them. We’re going to suggest types and functions to
+-- fill in order to accomplish the goal, but they’re not obligatory. If
+-- you want to do it differently…you do you.
+-- convo :: [String]
+-- convo =
+-- ["Wanna play 20 questions",
+-- "Ya",
+-- "U 1st haha",
+-- "Lol ok. Have u ever tasted alcohol lol",
+-- "Lol ya",
+-- "Wow ur cool haha. Ur turn",
+-- "Ok. Do u think I am pretty Lol",
+-- "Lol ya",
+-- "Haha thanks just making sure rofl ur turn"]
+-- -- validButtons = "1234567890*#"
+-- type Digit = Char
+-- -- Valid presses: 1 and up
+-- type Presses = Int
+-- reverseTaps :: DaPhone -> Char -> [(Digit, Presses)]
+-- reverseTaps = undefined
+-- -- assuming the default phone definition
+-- -- 'a' -> ('2', 1)
+-- -- 'A' -> [('*', 1), ('2', 1)]
+-- cellPhonesDead :: DaPhone
+-- -> String
+-- -> [(Digit, Presses)]
+-- cellPhonesDead = undefined
+-- 3. How many times do digits need to be pressed for each message?
+-- fingerTaps :: [(Digit, Presses)] -> Presses
+-- fingerTaps = undefined
+-- 4. What was the most popular letter for each message? What was
+-- its cost? You’ll want to combine reverseTaps and fingerTaps
+-- to figure out what it cost in taps. reverseTaps is a list because
+-- you need to press a different button in order to get capitals.
+-- mostPopularLetter :: String -> Char
+-- mostPopularLetter = undefined
+-- 5. What was the most popular letter overall? What was the most
+-- popular word?
+-- coolestLtr :: [String] -> Char
+-- coolestLtr = undefined
+-- coolestWord :: [String] -> String
+-- coolestWord = undefined
+
+
+------------------
+-- Hutton’s Razor
+------------------
+
+
+-- Hutton’s Razor10 is a very simple expression language that expresses
+-- integer literals and addition of values in that expression language.
+-- The “trick” to it is that it’s recursive and the two expressions you’re
+-- summing together could be literals or themselves further addition
+-- operations. This sort of datatype is stereotypical of expression languages
+-- used to motivate ideas in research papers and functional pearls.
+-- Evaluating or folding a datatype is also in some sense what you’re
+-- doing most of the time while programming anyway.
+-- 1. Your first task is to write the “eval” function which reduces an
+-- expression to a final sum.
+-- data Expr
+-- = Lit Integer
+-- | Add Expr Expr
+-- eval :: Expr -> Integer
+-- eval = error "do it to it"
+-- Example of expected output:
+-- Prelude> eval (Add (Lit 1) (Lit 9001))
+-- 9002
+-- 2. Write a printer for the expressions.
+-- printExpr :: Expr -> String
+-- printExpr = undefined
+-- Expected output:
+-- Prelude> printExpr (Add (Lit 1) (Lit 9001))
+-- "1 + 9001"
+-- Prelude> let a1 = Add (Lit 9001) (Lit 1)
+-- Prelude> let a2 = Add a1 (Lit 20001)
+-- Prelude> let a3 = Add (Lit 1) a2
+-- Prelude> printExpr a3
+-- "1 + 9001 + 1 + 20001"
