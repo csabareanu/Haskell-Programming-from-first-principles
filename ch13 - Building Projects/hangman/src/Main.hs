a0077e64f80e6ceb13af402f812144a32f9052f6
Ciprian Sabareanu <ciprian.sabareanu@opti.ro>
2018-11-07 17:15:57 +0200

    Chapter 13

--- ch13 - Building Projects/hangman/src/Main.hs
+++ ch13 - Building Projects/hangman/src/Main.hs
@@ -38,3 +38,34 @@ randomWord wl = do
 
 randomWord' :: IO String
 randomWord' = gameWords >>= randomWord
+
+data Puzzle = Puzzle String [Maybe Char] [Char]
+
+instance Show Puzzle where
+    show (Puzzle _ discovered guessed) =
+        (intersperse ' ' $ fmap renderPuzzleChar discovered)
+        ++ " Guessed so far: " ++ guessed
+
+freshPuzzle :: String -> Puzzle
+freshPuzzle s = Puzzle s (map (const Nothing) s) []
+
+charInWord :: Puzzle -> Char -> Bool
+charInWord (Puzzle s _ _) c = elem c s
+
+alreadyGuessed :: Puzzle -> Char -> Bool
+alreadyGuessed (Puzzle _ _ g) c = elem c g
+
+renderPuzzleChar :: Maybe Char -> Char
+renderPuzzleChar Nothing = '_'
+renderPuzzleChar (Just a) = a
+
+fillInCharacter :: Puzzle -> Char -> Puzzle
+fillInCharacter (Puzzle word filledInSoFar s) c =
+    Puzzle word newFilledInSoFar (c : s)
+    where
+            zipper guessed wordChar guessChar =
+                if wordChar == guessed
+                then Just wordChar
+                else guessChar
+            newFilledInSoFar =
+                zipWith (zipper c) word filledInSoFar
