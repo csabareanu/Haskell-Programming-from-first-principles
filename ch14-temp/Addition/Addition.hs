76759ca6496c39a5010d5c948a89320525efdbf3
Ciprian Sabareanu <ciprian.sabareanu@opti.ro>
2019-01-30 18:12:13 +0200

    more

--- ch14-temp/Addition/Addition.hs
+++ ch14-temp/Addition/Addition.hs
@@ -1,6 +1,7 @@
 module Addition where
 
 import Test.Hspec
+import Test.QuickCheck
 
 dividedBy :: Integral a => a -> a -> (a, a)
 dividedBy num denom = go num denom 0
@@ -20,6 +21,10 @@ main = hspec $ do
             (1 + 1) > 1 `shouldBe` True
         it "2 + 2 is equal to 4" $ do
             2 + 2 `shouldBe` 4
+        -- with QuickCheck
+        it "x + 1 is always greater than x" $ do
+            property $ \x -> x + 1 > (x :: Int)
+
     describe "Division" $ do
         it "15 divided by 3 is 5" $ do
             dividedBy 15 3 `shouldBe` (5,0)
@@ -30,3 +35,18 @@ main = hspec $ do
             multiplyBy 7 6 `shouldBe` 42
         it "6 multiplied by 0 is 0" $ do
             multiplyBy 6 0 `shouldBe` 0
+
+trivialInt :: Gen Int
+trivialInt = return 1
+
+oneThroughThree :: Gen Int
+oneThroughThree = elements [1,2,3]
+
+genBool :: Gen Bool
+genBool = choose (True, False)
+
+genTuple :: (Arbitrary a, Arbitrary b) => Gen (a, b)
+genTuple = do
+    a <- arbitrary
+    b <- arbitrary
+    return (a,b)
