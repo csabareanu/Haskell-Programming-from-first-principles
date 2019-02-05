4723e767412ba85c64c67716af8d33af9906a85c
Ciprian Sabareanu <ciprian.sabareanu@opti.ro>
2019-02-05 17:59:39 +0200

    Chapter 14-temp

--- ch14-temp/Exercises/Chapter_ex/General/General.hs
+++ ch14-temp/Exercises/Chapter_ex/General/General.hs
@@ -1,14 +1,24 @@
 ---------------------
 -- Using QuickCheck
 ---------------------
+import Test.QuickCheck
 import Data.List (sort)
 
+----------------------------------------------------------------
+-- Test some simple arithmetic properties using QuickCheck.
+----------------------------------------------------------------
 
-Test some simple arithmetic properties using QuickCheck.
 -- 1. -- for a function
 half x = x / 2
 -- this property should hold
+
 halfIdentity = (*2) . half
+
+prop_halfId :: Double -> Bool
+prop_halfId x = halfIdentity x == (x::Double)
+
+check_prop_halfId :: IO ()
+check_prop_halfId = quickCheck prop_halfId
 
 -- 2.
 -- for any list you apply sort to
@@ -19,111 +29,179 @@ listOrdered xs = snd $ foldr go (Nothing, True) xs
           go y (Nothing, t)      = (Just y, t)
           go y (Just x, t)       = (Just y, x >= y)
 
+prop_listOrdered :: [Int] -> Bool
+prop_listOrdered l = listOrdered . sort $ (l :: [Int])
+
+prop_listOrdered_string :: String -> Bool
+prop_listOrdered_string l = listOrdered . sort $ (l :: String)
+
+
+check_prop_listOrdered :: IO ()
+check_prop_listOrdered = quickCheck prop_listOrdered
+
+check_prop_listOrdered_string :: IO ()
+check_prop_listOrdered_string = quickCheck prop_listOrdered_string
+
 
 -- 3. Now we’ll test the associative and commutative properties of
 -- addition:
 plusAssociative x y z =
     x + (y + z) == (x + y) + z
+
+check_prop_assoc :: IO ()
+check_prop_assoc = quickCheck (plusAssociative :: Int->Int->Int->Bool)
+
 plusCommutative x y =
     x + y == y + x
 
+check_prop_comm :: IO ()
+check_prop_comm = quickCheck (plusCommutative :: Int->Int->Bool)
 
--- 4. Now do the same for multiplication.
+
+
+-- -- 4. Now do the same for multiplication.
+multAssoc a b c =
+    a * (b * c) == (a * b) * c
+
+multComm a b =
+    a * b == b * a
+
+check_mult_assoc :: IO()
+check_mult_assoc = quickCheck (multAssoc :: Int->Int->Int->Bool)
+
+check_mult_comm :: IO()
+check_mult_comm = quickCheck (multComm :: Int->Int->Bool)
 
 
 -- 5. We mentioned in one of the first chapters that there are some
 -- laws involving the relationship of quot and rem and div and mod.
 -- Write QuickCheck tests to prove them.
 -- quot rem
-(quot x y)*y + (rem x y) == x
-(div x y)*y + (mod x y) == x
+-- (quot x y)*y + (rem x y) == x
+-- (div x y)*y + (mod x y) == x
+
+prop_quotrem1 :: Int -> NonZero Int -> Bool
+prop_quotrem1 x (NonZero y) = (quot x y) * y + (rem x y) == x
+
+prop_quotrem2 :: Int -> NonZero Int -> Bool
+prop_quotrem2 x (NonZero y) = (div x y) * y + (mod x y) == x
+
+check_quotrem1 :: IO()
+check_quotrem1 = quickCheck prop_quotrem1
+
+check_quotrem2 :: IO()
+check_quotrem2 = quickCheck prop_quotrem2
 
 
+-- -- 6. Is (^) associative? Is it commutative? Use QuickCheck to see if
+-- -- the computer can contradict such an assertion.
 
--- 6. Is (^) associative? Is it commutative? Use QuickCheck to see if
--- the computer can contradict such an assertion.
+prop_pow_assoc :: Int -> Int -> Int-> Bool
+prop_pow_assoc a b c = (a^b)^c == a^(b^c)
+
+prop_pow_comm :: Int -> Int -> Bool
+prop_pow_comm a b = (a^b) == (b^a)
+
+check_pow_assoc :: IO()
+check_pow_assoc = quickCheck prop_pow_assoc
+
+check_pow_comm :: IO()
+check_pow_comm = quickCheck prop_pow_comm
 
 
+-- -- 7. Test that reversing a list twice is the same as the identity of the
+-- -- list:
+-- -- reverse . reverse == id
+prop_2_reverse :: [Int] -> Bool
+prop_2_reverse xs = (==) (id xs) (reverse . reverse $ xs)
 
--- 7. Test that reversing a list twice is the same as the identity of the
--- list:
-reverse . reverse == id
 
+check_2_reverse :: IO()
+check_2_reverse = quickCheck prop_2_reverse
 
 
 -- 8. Write a property for the definition of ($).
-f $ a = f a
-f . g = \x -> f (g x)
+-- f $ a = f a
+-- f . g = \x -> f (g x)
+
+prop_dollar :: (Eq b) => (a -> b) -> a -> Bool
+prop_dollar f x = f x == (f $ x)
+
+check_dollar_1 :: IO()
+check_dollar_1 = quickCheck (prop_dollar id :: Int -> Bool)
+
+check_dollar_2 :: IO()
+check_dollar_2 = quickCheck (prop_dollar length :: [Int] -> Bool)
 
 
 
--- 9. See if these two functions are equal:
-foldr (:) == (++)
-foldr (++) [] == concat
+-- -- 9. See if these two functions are equal:
+-- -- foldr (:) == (++)
+-- -- foldr (++) [] == concat
 
 
--- 10. Hm. Is that so?
-f n xs = length (take n xs) == n
+-- -- 10. Hm. Is that so?
+-- f n xs = length (take n xs) == n
 
 
 
--- 11. Finally, this is a fun one. You may remember we had you compose
--- read and show one time to complete a “round trip.” Well,
--- now you can test that it works:
-f x = (read (show x)) == x
+-- -- 11. Finally, this is a fun one. You may remember we had you compose
+-- -- read and show one time to complete a “round trip.” Well,
+-- -- now you can test that it works:
+-- f x = (read (show x)) == x
 
 
------------
---Failure
------------
+-- -----------
+-- --Failure
+-- -----------
 
--- Find out why this property fails.
--- for a function
-square x = x * x
--- why does this property not hold? Examine the type of sqrt.
-squareIdentity = square . sqrt
--- Hint: Read about floating point arithmetic and precision if you’re
--- unfamiliar with it.
+-- -- Find out why this property fails.
+-- -- for a function
+-- square x = x * x
+-- -- why does this property not hold? Examine the type of sqrt.
+-- squareIdentity = square . sqrt
+-- -- Hint: Read about floating point arithmetic and precision if you’re
+-- -- unfamiliar with it.
 
 
----------------
---Idempotence
----------------
+-- ---------------
+-- --Idempotence
+-- ---------------
 
--- Idempotence refers to a property of some functions in which the
--- result value does not change beyond the initial application. If you
--- apply the function once, it returns a result, and applying the same
--- function to that value won’t ever change it. You might think of a list
--- that you sort: once you sort it, the sorted list will remain the same
--- after applying the same sorting function to it. It’s already sorted, so
--- new applications of the sort function won’t change it.
--- Use QuickCheck and the following helper functions to demonstrate
--- idempotence for the following:
-twice f = f . f
-fourTimes = twice . twice
--- 1.
-f x =
-    capitalizeWord x
-        == twice capitalizeWord x
-        == fourTimes capitalizeWord x
--- 2.
-f x =
-    sort x
-        == twice sort x
-        == fourTimes sort x
+-- -- Idempotence refers to a property of some functions in which the
+-- -- result value does not change beyond the initial application. If you
+-- -- apply the function once, it returns a result, and applying the same
+-- -- function to that value won’t ever change it. You might think of a list
+-- -- that you sort: once you sort it, the sorted list will remain the same
+-- -- after applying the same sorting function to it. It’s already sorted, so
+-- -- new applications of the sort function won’t change it.
+-- -- Use QuickCheck and the following helper functions to demonstrate
+-- -- idempotence for the following:
+-- twice f = f . f
+-- fourTimes = twice . twice
+-- -- 1.
+-- f x =
+--     capitalizeWord x
+--         == twice capitalizeWord x
+--         == fourTimes capitalizeWord x
+-- -- 2.
+-- f x =
+--     sort x
+--         == twice sort x
+--         == fourTimes sort x
 
--- Make a Gen random generator for the datatype
--- We demonstrated in the chapter how to make Gen generators for
--- different datatypes. We are so certain you enjoyed that, we are going
--- to ask you to do it for some new datatypes:
--- 1. Equal probabilities for each.
-data Fool =
-        Fulse
-        | Frue
-    deriving (Eq, Show)
+-- -- Make a Gen random generator for the datatype
+-- -- We demonstrated in the chapter how to make Gen generators for
+-- -- different datatypes. We are so certain you enjoyed that, we are going
+-- -- to ask you to do it for some new datatypes:
+-- -- 1. Equal probabilities for each.
+-- data Fool =
+--         Fulse
+--         | Frue
+--     deriving (Eq, Show)
 
--- 2. 2/3s chance of Fulse, 1/3 chance of Frue.
-data Fool =
-        Fulse
-        | Frue
-    deriving (Eq, Show)
+-- -- 2. 2/3s chance of Fulse, 1/3 chance of Frue.
+-- -- data Fool =
+-- --         Fulse
+-- --         | Frue
+-- --     deriving (Eq, Show)
