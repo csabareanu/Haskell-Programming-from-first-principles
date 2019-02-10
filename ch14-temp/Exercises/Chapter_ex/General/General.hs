9ccf025b9c8ce594e1767997ef60b54c484b0e9d
csabareanu <ciprian.sabareanu@gmail.com>
2019-02-10 16:27:12 +0200

    Chapter 14 temo

--- ch14-temp/Exercises/Chapter_ex/General/General.hs
+++ ch14-temp/Exercises/Chapter_ex/General/General.hs
@@ -248,13 +248,67 @@ sort_test = quickCheck prop_sort
 -- -- different datatypes. We are so certain you enjoyed that, we are going
 -- -- to ask you to do it for some new datatypes:
 -- -- 1. Equal probabilities for each.
--- data Fool =
---         Fulse
-----         | Frue
---     deriving (Eq, Show)
+data Fool =
+    Fulse
+    | Frue
+        deriving (Eq, Show)
 
--- -- 2. 2/3s chance of Fulse, 1/3 chance of Frue.
--- -- data Fool =
--- --         Fulse
--- --         | Frue
--- --     deriving (Eq, Show)
+genFool :: Gen Fool
+genFool = elements [Fulse, Frue]
+
+
+-- 2. 2/3s chance of Fulse, 1/3 chance of Frue.
+
+genFool2 :: Gen Fool
+genFool2 =
+    frequency [(1, return Frue)
+              ,(2, return Fulse)
+            ]
+
+-------------------
+--Hangman testing
+-------------------
+
+-- Next, you should go back to the Hangman project from the previous
+-- chapter and write tests. The kinds of tests you can write at this point
+-- will be limited due to the interactive nature of the game. However, you
+-- can test the functions. Focus your attention on testing the following:
+data Puzzle = Puzzle String [Maybe Char] [Char]
+
+charInWord :: Puzzle -> Char -> Bool
+charInWord (Puzzle s _ _) c = elem c s
+
+alreadyGuessed :: Puzzle -> Char -> Bool
+alreadyGuessed (Puzzle _ _ g) c = elem c g
+
+fillInCharacter :: Puzzle -> Char -> Puzzle
+fillInCharacter (Puzzle word filledInSoFar s) c =
+    Puzzle word newFilledInSoFar (c : s)
+    where
+        zipper guessed wordChar guessChar =
+            if wordChar == guessed
+            then Just wordChar
+            else guessChar
+        newFilledInSoFar =
+            zipWith (zipper c) word filledInSoFar
+
+
+handleGuess :: Puzzle -> Char -> IO Puzzle
+handleGuess puzzle guess = do
+    putStrLn $ "Your guess was: " ++ [guess]
+    case (charInWord puzzle guess, alreadyGuessed puzzle guess) of
+        (_, True) -> do
+            putStrLn "You already guessed that\
+                \ character, pick something else!"
+            return puzzle
+        (True, _) -> do
+            putStrLn "This character was in the word,\
+                \ filling in the word accordingly"
+            return (fillInCharacter puzzle guess)
+        (False, _) -> do
+            putStrLn "This character wasn't in\
+                \ the word, try again."
+            return (fillInCharacter puzzle guess)
+
+-- Refresh your memory on what those are supposed to do and then
+-- test to make sure they do.
